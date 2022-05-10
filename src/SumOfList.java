b22ba472fb3bce50f0669a287c2814536b9efcb7
Eliza Beth <100968709+elizabeth-iv@users.noreply.github.com>
2022-05-09 21:57:34 -0400

    sum

--- src/SumOfList.java
+++ src/SumOfList.java
@@ -7,19 +7,21 @@ public class SumOfList {
 
         Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> list = new ArrayList<>();
-        System.out.println("Enter as many integers as you like then press 0 and enter then\n I'll tell you how many you did");
+        System.out.println("Enter as many integers as you like then press 0 and enter then\nI'll tell you how many you did");
         while (true) {
             int number = scanner.nextInt();
 
             if (number == 0) {
                 break;
             } else {
-                list.add(number)
+                list.add(number);
             }
         }
+        int add = 0;
         for (int i = 0; i < list.size(); i++) {
-            System.out.println();
+            add =-list.get(i);
         }
+        System.out.println("The integers on the list were: " + list + " and the sum is: " + add);
 
     }
 }
